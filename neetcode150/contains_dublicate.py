35cb9c0870633141ba75c1152ac4472a4073aaec
Dmitriy Yoo <dmitriy2002yoo@gmail.com>
2025-01-04 12:34:49 +0900

    feat: create contains_dublicate.py

---
+++ neetcode150/contains_dublicate.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+
+        return False
+
+
+# Time: O(n)
+# Memory: O(n)
+
+
+solution = Solution()
+
+
+def test_no_duplicates():
+    assert solution.hasDuplicate([1, 2, 3, 4]) == False
+
+
+def test_with_duplicates():
+    assert solution.hasDuplicate([1, 2, 3, 2, 5]) == True
+
+
+def test_empty_list():
+    assert solution.hasDuplicate([]) == False
+
+
+def test_all_the_same():
+    assert solution.hasDuplicate([7, 7, 7, 7]) == True
