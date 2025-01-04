e8fa0b21125743b23567c55a922b4fd9613fa8b1
Dmitriy Yoo <dmitriy2002yoo@gmail.com>
2025-01-04 12:53:52 +0900

    feat: create 3_two_sum.py

---
+++ neetcode150/3_two_sum.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+# Task: https://neetcode.io/problems/two-integer-sum
+# Solution
+# Time: O(n)
+# Memory: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+
+
+solution = Solution()
+
+
+def test_two_sum():
+    assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert solution.twoSum([3, 2, 4], 6) == [1, 2]
+    assert solution.twoSum([3, 3], 6) == [0, 1]
